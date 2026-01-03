6519d0c23e2dd90e91f7d278da810ba96fc3635b
Sunilkumarqv <sunilkumarqv@gmail.com>
2026-01-03 18:01:56 +0000

    refactor: update resource group configuration and provider settings

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,10 +1,16 @@
-# Create a random name for the resource group using random_pet
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# Create a resource group using the generated random name
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "example" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = "example-resources"
+  location = "West Europe"
 }
