7c367bbc4aae291beab37a3ab26627f88ab85e3c
Sunilkumarqv <163056681+Sunilkumarqv@users.noreply.github.com>
2026-01-04 11:23:12 +0000

    Refactor resource group creation in main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,70 +2,10 @@
 # resource "random_pet" "rg_name" {
 #   prefix = var.resource_group_name_prefix
 # }
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-# Random suffix for unique naming
-resource "random_string" "suffix" {
-  length  = 5
-  upper   = false
-  lower   = true
-  special = false
-}
-
 
 # Create a resource group using the generated random name
-resource "azurerm_resource_group" "rg" {
-  location = var.location
+resource "azurerm_resource_group" "example" {
+  location = var.resource_group_location
   #name     = random_pet.rg_name.id
-  name     = "${var.resource_group_name}-${random_string.suffix.result}"
+  name     = var.resource_group_name
 } 
-
-# Storage Account (ADLS Gen2)
-module "adls" {
-  source              = "./modules/adls"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  storage_account_name = "adls${random_string.suffix.result}"
-}
-# Key Vault
-module "key_vault" {
-  source              = "./modules/key_vault"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  keyvault_name       = "kv-${random_string.suffix.result}"
-}
-
-
-# Data Factory
-module "data_factory" {
-  source              = "./modules/data_factory"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  adf_name            = "adf-${random_string.suffix.result}"
-}
-
-
-# Databricks
-module "databricks" {
-  source                   = "./modules/databricks"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  databricks_workspace_name = "dbw-${random_string.suffix.result}"
-}
-
-
