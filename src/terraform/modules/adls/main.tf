62711be0924257e516ba906c9899f48c7082be8b
Sunilkumarqv <163056681+Sunilkumarqv@users.noreply.github.com>
2026-01-04 12:30:47 +0000

    stoage

--- src/terraform/modules/adls/main.tf
+++ src/terraform/modules/adls/main.tf
@@ -4,21 +4,25 @@ resource "azurerm_storage_account" "adls" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  # ADLS Gen2
+  account_kind             = "StorageV2"
   is_hns_enabled           = true
+
+  tags = {
+    environment = var.environment
+  }
 }
 
 resource "azurerm_storage_container" "bronze" {
-  name                  = "bronze"
-  storage_account_id    = azurerm_storage_account.adls.id
+  name                  = var.container_name
+  storage_account_id  = azurerm_storage_account.adls.id
   container_access_type = "private"
-}   
+}
+
 resource "azurerm_storage_container" "silver" {
-  name                  = "silver"
-  storage_account_id    = azurerm_storage_account.adls.id
+  name                  = var.container_name_silver
+  storage_account_id  = azurerm_storage_account.adls.id
   container_access_type = "private"
 }
-resource "azurerm_storage_container" "gold" {
-  name                  = "gold"
-  storage_account_id    = azurerm_storage_account.adls.id
-  container_access_type = "private"
-}
+
--- src/terraform/modules/adls/variables.tf
+++ src/terraform/modules/adls/variables.tf
@@ -1,3 +1,19 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "storage_account_name" {}
+variable "resource_group_name" {
+  description = "Name of the existing resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Globally unique name for the storage account"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment tag value"
+  type        = string
+}
