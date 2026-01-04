258c59aaf7e36791958e025581249270dc642c30
Sunilkumarqv <163056681+Sunilkumarqv@users.noreply.github.com>
2026-01-04 12:56:30 +0000

    changes

--- src/terraform/modules/adls/main.tf
+++ src/terraform/modules/adls/main.tf
@@ -1,11 +1,9 @@
 resource "azurerm_storage_account" "adls" {
-  name                     = var.storage_account_name
+  name                     = var.adls_storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  # ADLS Gen2
   account_kind             = "StorageV2"
   is_hns_enabled           = true
 
@@ -15,14 +13,13 @@ resource "azurerm_storage_account" "adls" {
 }
 
 resource "azurerm_storage_container" "bronze" {
-  name                  = var.container_name
-  storage_account_id  = azurerm_storage_account.adls.id
+  name                  = var.adls_container_name
+  storage_account_id    = azurerm_storage_account.adls.id
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "silver" {
-  name                  = var.container_name_silver
-  storage_account_id  = azurerm_storage_account.adls.id
+  name                  = var.adls_container_name_silver
+  storage_account_id    = azurerm_storage_account.adls.id
   container_access_type = "private"
 }
-
