a57a7f0f576215e790048605f08dc1a4d700a444
Sunilkumarqv <163056681+Sunilkumarqv@users.noreply.github.com>
2026-01-04 11:24:01 +0000

    Add Terraform provider configurations for Azure and random

--- src/terraform/providers.tf
+++ src/terraform/providers.tf
@@ -1,2 +1,39 @@
-variable "subscription_id" {}
-variable "tenant_id" {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# # Create a resource group
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = azurerm_resource_group.example.location
+# }
+
+
+# # create a datafactory instance in the resource group created above
+# resource "azurerm_data_factory" "example" {
+#   name                = "example-datafactory-${random_pet.rg_name.id}"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+
+# # create a azure databricks workspace in the resource group created above
+# resource "azurerm_databricks_workspace" "example" {
+#   name                        = "example-databricks-${random_pet.rg_name.id}"
+#   location                    = azurerm_resource_group.example.location
+#   resource_group_name         = azurerm_resource_group.example.name
+#   sku                         = "standard"
+#   managed_resource_group_name = "example-managed-rg-${random_pet.rg_name.id}"
+# }
