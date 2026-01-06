cac62c3e018167c01631c118702582aba9837493
Sunilkumarqv <163056681+Sunilkumarqv@users.noreply.github.com>
2026-01-06 17:46:02 +0000

    test11

--- src/snowflake/run_snowflake.py
+++ src/snowflake/run_snowflake.py
@@ -22,16 +22,26 @@ ctx = snowflake.connector.connect(
 
 print("Connected successfully.")
 
-try:
-    with open("src/snowflake/retail_copy.sql", "r") as sql_file:
-        cur = ctx.cursor()
-        try:
-            sql_commands = sql_file.read()
-            cur.execute(sql_commands)
-            print("SQL script executed successfully.")
-        finally:
-            cur.close()
-finally:
-    ctx.close()
-    print("Connection closed.")
-    
+#read and execute the SQL file
+# with open("src/snowflake/snowflake.sql", "r") as file:
+#     sql_commands = file.read() 
+#     with ctx.cursor() as cs:
+#         cs.execute(sql_commands)
+#         print("SQL script executed successfully.")
+# ctx.close()
+
+#remove /* comments from the SQL file
+with open("src/snowflake/snowflake.sql", "r") as file:
+    sql_content = file.read()   
+    import re
+    cleaned_sql = re.sub(r'/\*.*?\*/', '', sql_content, flags=re.DOTALL)
+    with open("src/snowflake/snowflake_cleaned.sql", "w") as cleaned_file:
+        cleaned_file.write(cleaned_sql)
+print("Comments removed and cleaned SQL file created.")
+
+#reand and execute the cleaned SQL file
+with open("src/snowflake/snowflake_cleaned.sql", "r") as file:
+    sql_commands = file.read() 
+    with ctx.cursor() as cs:
+        cs.execute(sql_commands)
+        print("Cleaned SQL script executed successfully.")  
